f4380c3901e4af395ad2b7c58c4ede60e8230dd9
athegemann <ahegemann@gmail.com>
2026-04-15 15:52:04 -0300

    implementado test mean decimal

--- tests/test_mean.py
+++ tests/test_mean.py
@@ -29,3 +29,6 @@ def test_mean_lista_un_elemento():
 
 def test_mean_lista_negativa():
     assert mean([-5,-3,-1]) == -3
+
+def test_mean_lista_decimal():
+    assert mean([3.8,2.9,9.3,5.6]) == 5.4
